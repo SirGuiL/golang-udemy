871c22c3aea3fff0c297bd5141a01f8eca073407
Guilherme Lima <guibiel-10@hotmail.com>
2024-03-29 00:49:48 -0300

    finished arrays and slices class

---
+++ arrays-and-slices/arrays-and-slices.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(1 + 2)
+
+	var arr1 [5]int
+
+	fmt.Println(arr1)
+
+	arr1[0] = 1
+	arr1[1] = 2
+	arr1[2] = 3
+	arr1[3] = 4
+	arr1[4] = 5
+
+	fmt.Println(arr1)
+
+	arr2 := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(arr2)
+
+	slice := []int{1, 2, 3, 4, 5}
+	fmt.Println(slice)
+
+
+	slice = append(slice, 6)
+	fmt.Println(slice)
+
+	slice2 := arr2[1:3]
+	fmt.Println(slice2)
+}
