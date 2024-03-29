5dfdb4111ad0896d24f9af188bdc03e5336df493
Guilherme Lima <guibiel-10@hotmail.com>
2024-03-28 23:26:31 -0300

    finished operator class

---
+++ operators/operators.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(1 + 2)
+	fmt.Println(1 - 2)
+	fmt.Println(1 * 2)
+	fmt.Println(1 / 2)
+	fmt.Println(1 % 2)
+	fmt.Println(1 == 2)
+	fmt.Println(1 != 2)
+	fmt.Println(1 < 2)
+	fmt.Println(1 > 2)
+	fmt.Println(1 <= 2)
+	fmt.Println(1 >= 2)
+}
